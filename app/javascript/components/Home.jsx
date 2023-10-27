4fe62b32302d0feab3573e51d2619e3599ef538c
Ryan Kam <rkam0725@gmail.com>
2023-10-27 10:20:51 +0900

    setup React component structure

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Welcome to Japan Hunters!</h1>
+        <p className="lead">
+            Have someone in Japan hunt down that rare item for you.
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/bounties"
+          className="btn btn-warning btn-lg"
+          role="button"
+        >
+          Check out Bounties
+        </Link>
+      </div>
+    </div>
+  </div>
+);
