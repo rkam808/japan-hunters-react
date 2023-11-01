f94dd760a5d44312970a0df76c527dd62b6e3726
Ryan Kam <rkam0725@gmail.com>
2023-11-01 18:08:19 +0900

    new and create for bounty

--- app/controllers/bounties_controller.rb
+++ app/controllers/bounties_controller.rb
@@ -3,4 +3,29 @@ class BountiesController < ApplicationController
         bounties = Bounty.all.order(created_at: :desc)
         render json: bounties
     end
+
+    def new
+        @bounty = Bounty.new
+        # authorize @bounty - for pundit
+    end
+
+    def create
+        @bounty = Bounty.new(bounty_params)
+        @bounty.user = current_user
+
+        # new_item = Item.new()
+        # authorize @bounty - for pundit
+
+        if @bounty.save
+            redirect_to bounty_path(@bounty)
+        else
+            redirect_to new_bounty_path
+        end
+    end
+
+    private
+
+    def bounty_params
+        params.require(:bounty).permit(:description, :price, :item, :photo)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Routes for Models
   resources :items, only: [:index, :show, :create]
-  resources :bounties, only: [:index, :show, :create, :update, :destroy]
+  resources :bounties, only: [:index, :show, :new, :create, :update, :destroy]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
