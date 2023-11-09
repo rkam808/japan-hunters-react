5ec45ffde20fa627d8bd04c44ee447e9fc4a4504
Ryan Kam <rkam0725@gmail.com>
2023-11-09 15:26:41 +0900

    fix bounty image on frontend

--- app/javascript/components/Bounties.jsx
+++ app/javascript/components/Bounties.jsx
@@ -22,12 +22,13 @@ const Bounties = () => {
     <div key={index} className="col-md-6 col-lg-4">
       <div className="card mb-4">
         <img
-          src={bounty.image}
+          src={bounty.photo_url}
           className="card-img-top"
-          alt={`${bounty.name} image`}
+          alt={`${bounty.description} image`}
         />
         <div className="card-body">
-          <h5 className="card-title">{bounty.name}</h5>
+          <h5 className="card-title">{bounty.description}</h5>
+          <p>Offer: {bounty.price} JPY</p>
           <Link to={`/bounty/${bounty.id}`} className="btn custom-button">
             View Bounty
           </Link>
@@ -48,27 +49,24 @@ const Bounties = () => {
     <>
       <section className="jumbotron jumbotron-fluid text-center">
         <div className="container py-5">
-          <h1 className="display-4">Bounties for every occasion</h1>
+          <h1 className="display-4">Bounty Board</h1>
           <p className="lead text-muted">
-            We’ve pulled together our most popular recipes, our latest
-            additions, and our editor’s picks, so there’s sure to be something
-            tempting for you to try.
+            Bounties that users have posted. If you're a Hunter, browse the open bounties and see if there are any you can fulfill!
+            If you're a Collector, why not post a Bounty and get in touch with a Hunter?
           </p>
         </div>
       </section>
       <div className="py-5">
         <main className="container">
           <div className="text-end mb-3">
-            <Link to="/bounty" className="btn custom-button">
-              Create New Bounty
-            </Link>
+            {/* <Link to="/bounties/new" className="btn btn-warning custom-button">
+              Post a Bounty
+            </Link> */}
+            <a href="/bounties/new">Post a Bounty</a>
           </div>
           <div className="row">
             {bounties.length > 0 ? allBounties : noBounty}
           </div>
-          <Link to="/" className="btn btn-link">
-            Home
-          </Link>
         </main>
       </div>
     </>
